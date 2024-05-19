dc6126672fbaa8dc9ed6c5e5c46b0b5d24576d8d
algomachine001 <amaben202phantom@gmail.com>
2024-05-19 17:17:38 +0100

    using the storage stack in api gateway

--- stacks/APIStack.ts
+++ stacks/APIStack.ts
@@ -1,7 +1,14 @@
-import { Api, StackContext } from 'sst/constructs';
+import { Api, StackContext, use } from 'sst/constructs';
+import { StorageStack } from './StorageStack';
 
 export function APIStack({ stack }: StackContext) {
+  const { table } = use(StorageStack);
   const api = new Api(stack, 'api', {
+    defaults: {
+      function: {
+        bind: [table],
+      },
+    },
     routes: {
       'GET /': 'packages/functions/src/lambda.handler',
       'GET /test': 'packages/functions/src/test/lambda.handler',
--- stacks/StorageStack.ts
+++ stacks/StorageStack.ts
@@ -1,15 +1,21 @@
-import { StackContext, Table } from 'sst/constructs';
+import { Bucket, StackContext, Table } from 'sst/constructs';
 
 export function StorageStack({ stack }: StackContext) {
+  // Create an S3 bucket amd DynamoDB Table
+  const bucket = new Bucket(stack, 'Uploads');
   const table = new Table(stack, 'Notes', {
     fields: {
       userId: 'string',
       noteId: 'string',
+      name: 'string',
+      age: 'number',
+      createdAt: 'binary',
     },
     primaryIndex: { partitionKey: 'userId', sortKey: 'noteId' },
   });
 
   return {
     table,
+    bucket,
   };
 }
